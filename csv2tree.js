ec970fe1c08e847c6ab3fecb83693ce0b77b48be
jcreager <josephcreager@gmail.com>
2016-09-13 20:15:28 -0700

    Initial Commit
    This is the first working version of the code.  it will transform a
    flat csv file with with a unique identifier for each row, and a
    reference to each parent relationship represented in another row, where
    the parent relationship to each child is represented by a parent id
    reference for each record in every row where the record has a parent
    relationship.

---
+++ csv2tree.js
@@ -0,0 +1,57 @@
+var fs = require('fs')
+
+function csvToObj (csv) {
+  var arr = csv.split('\n')
+  var jsonObj = []
+  var headers = arr[0].split(',')
+
+  for (var i = 1; i < arr.length; i++) {
+    var data = arr[i].split(',')
+    var obj = {}
+
+    for (var j = 0; j < data.length; j++) {
+      obj[headers[j].trim()] = data[j].trim()
+    }
+    jsonObj.push(obj)
+  }
+  return JSON.stringify(jsonObj)
+}
+
+function convertType (value) {
+  try {
+    return (new Function ('return ' + value + ';'))()
+  } catch (e) {
+    return value
+  }
+}
+
+function treeify (nodes) {
+  var indexed_nodes = {}
+  var tree_roots = []
+  for (var i = 0; i < nodes.length; i += 1) {
+    indexed_nodes[nodes[i].id] = nodes[i]
+    indexed_nodes[nodes[i].id].children = []
+  }
+  for (var i = 0; i < nodes.length; i += 1) {
+    var parent_id = nodes[i].parent_id
+    if (convertType(parent_id) === undefined || parent_id === nodes[i].id) {
+      tree_roots.push(nodes[i])
+    } else {
+      if (indexed_nodes[parent_id] !== undefined) {
+        indexed_nodes[parent_id].children.push(nodes[i])
+     } else {
+       tree_roots.push(nodes[i])
+       nodes[i].warnings = 'parent_id for ' + nodes[i].name + ' does not exist in data'
+      }
+    }
+  }
+  return tree_roots
+}
+
+fs.readFile('./data.csv', function (err, data) {
+  if (err) {
+    console.log(err)
+  }
+  var flatArray = csvToObj(data.toString())
+  console.log(JSON.stringify(treeify(JSON.parse(flatArray)), undefined, "\t"))
+})
