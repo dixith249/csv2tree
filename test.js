0ce8953d281ab74799daf127ddc810e3f6f77f67
jcreager <josephcreager@gmail.com>
2016-09-15 21:38:57 -0700

    added modules to support options
    Added modules required to support including command line options.

--- test.js
+++ test.js
@@ -1,8 +1,34 @@
 var csv2tree = require('./index.js')
+var program = require('commander')
+var path = require('path')
+var treeify = require('treeify')
 
-csv2tree('./data.csv', function (err, tree) {
-  if (err) {
-    console.log(err)
+var args = process.argv
+
+program
+  .version('1.0.0')
+  .option('-t, --tree', 'Unix style tree-view')
+  .parse(args)
+
+var count = 0
+for (var i = 0; i < args.length; i++) {
+  if (path.extname(args[i]) === '.csv') {
+    var data = args[i]
+  } else {
+    if (count < 1) {
+      console.log('Please provide the path to a .csv file')
+      count++
+    }
   }
-  console.log(tree)
-})
+}
+
+if (data) {
+  csv2tree(data, function (err, tree) {
+    if (err) {
+      console.log(err)
+    }
+    console.log(tree,
+       treeify.asTree(JSON.parse(tree), true)
+    )
+  })
+}
